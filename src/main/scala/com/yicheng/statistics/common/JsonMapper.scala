b6f339966df17e759e04001ff267759bc0cfd01c
yuer <983312113@qq.com>
2016-12-16 18:15:32 +0800

    新增casssndra实体类依赖

---
+++ src/main/scala/com/yicheng/statistics/common/JsonMapper.scala
@@ -0,0 +1,11 @@
+package com.yicheng.statistics.common
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+
+/**
+  * Created by yuer on 2016/12/16.
+  */
+object JsonMapper extends ObjectMapper with ScalaObjectMapper {
+
+}
